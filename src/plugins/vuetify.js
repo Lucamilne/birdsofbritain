d0c76b75989e9fcf46de2c4ff282ec167b845fe9
lucamilne <lucamilne@googlemail.com>
2021-10-23 17:52:50 +0100

    Hero image updated

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -11,7 +11,7 @@ export default new Vuetify({
                 secondary: "#546747",
                 tertiary: "#BBC8BA",
                 light: "#eee",
-                dark: "#fff"
+                dark: "#222"
             },
         },
     },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,24 @@
 <template>
-  <v-container fluid>
-    <v-card tile dark color="secondary">
-      <v-list-item three-line>
-        <v-list-item-content>
-          <div class="text-overline mb-4">Discover</div>
-          <v-list-item-title class="text-h5 mb-1"> Birds of Britain. </v-list-item-title>
-          <v-list-item-subtitle
-            >A comprehensive identifier of both native and migratory
-            birds.</v-list-item-subtitle
-          >
-        </v-list-item-content>
-      </v-list-item>
+  <v-container fluid class="fill-height">
+    <v-card
+      tile
+      dark
+      height="100%"
+      width="100%"
+      class="hero-background d-flex flex-column justify-space-between"
+    >
+      <v-card-text class="text-overline">Discover </v-card-text>
+      <v-card-text>
+        <v-img max-width="60%" :src="require('../assets/text/title.webp')"></v-img>
+        <v-list-item-subtitle class="mt-2 text-wrap"
+          >A comprehensive identifier of both native and migratory
+          birds.</v-list-item-subtitle
+        >
+      </v-card-text>
       <v-card-actions>
         <v-btn
           outlined
-          color="tertiary"
+          color="light"
           v-for="item in items"
           :key="item.name"
           link
@@ -25,7 +29,7 @@
           "
         >
           {{ item.name }}
-          <v-icon right>{{ item.icon }}</v-icon>
+          <!-- <v-icon right>{{ item.icon }}</v-icon> -->
         </v-btn>
       </v-card-actions>
     </v-card>
@@ -41,7 +45,31 @@ export default {
         { name: "Identify", icon: "mdi-magnify", route: "/identify" },
         { name: "Browse", icon: "mdi-binoculars", route: "/browse" },
       ],
+      images: [
+        { alt: "magpie", path: "magpie.jpg"},
+        { alt: "robin", path: "robin.jpg"},
+        { alt: "blackbird", path: "blackbird.jpg"},
+        { alt: "kingfisher", path: "kingfisher.jpg"}
+      ]
     };
   },
 };
 </script>
+
+<style scoped>
+.hero-background {
+  background-image: linear-gradient(
+      to bottom,
+      rgba(187, 200, 186, 0.6) 0%,
+      rgba(84, 103, 71, 0.6) 100%
+    ),
+    url("../assets/images/kingfisher.jpg");
+  background-blend-mode: soft-light;
+  background-size: cover;
+  background-position: center center;
+  padding: 2rem;
+}
+.hero-title {
+  font-size: 12rem;
+}
+</style>
