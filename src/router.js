2a8b05403993d60550ecc72032497f332d66f063
lucamilne <lucamilne@googlemail.com>
2021-10-24 11:07:07 +0100

    removed history mode from router

--- src/router.js
+++ src/router.js
@@ -11,7 +11,7 @@ import Favourites from "./views/Favourites.vue";
 Vue.use(Router);
 
 export default new Router({
-    mode: 'history',
+    // mode: 'history',
     routes: [
         {
             path: "/",
