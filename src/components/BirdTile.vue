e48d707c1f94b0e9ad462e1d6a8a80edb15c48be
lmilneCC <lmilne@cryptocompare.com>
2021-10-28 17:57:29 +0100

    Bird tile ripple

--- src/components/BirdTile.vue
+++ src/components/BirdTile.vue
@@ -1,28 +1,23 @@
 <template>
-  <v-card tile>
-    <v-list-item three-line>
-      <v-list-item-content>
-        <div class="overline mb-4">
-          {{ birds[bird].population.type }}
-        </div>
-        <v-list-item-title class="headline mb-1">
-          {{ birds[bird].name }}
-        </v-list-item-title>
-        <v-list-item-subtitle>{{ birds[bird].scientificName }}</v-list-item-subtitle>
-      </v-list-item-content>
-      <router-link :to="`/birds/${toKebabCase(bird)}`">
-        <v-list-item-avatar tile size="120" color="grey lighten-4">
-          <img
-            :src="require(`../assets/birds/${birds[bird].images[0]}`)"
-            :alt="birds[bird].name"
-          />
-        </v-list-item-avatar>
-      </router-link>
-    </v-list-item>
-    <v-card-actions>
-      <v-btn text color="primary" :to="`/birds/${toKebabCase(bird)}`">Learn more</v-btn>
-    </v-card-actions>
-  </v-card>
+    <v-card v-ripple tile :to="`/birds/${toKebabCase(bird)}`">
+        <v-list-item three-line>
+            <v-list-item-content>
+                <div class="overline mb-4">
+                    {{ birds[bird].population.type }}
+                </div>
+                <v-list-item-title class="headline mb-1">
+                    {{ birds[bird].name }}
+                </v-list-item-title>
+                <v-list-item-subtitle>{{ birds[bird].scientificName }}</v-list-item-subtitle>
+            </v-list-item-content>
+            <v-list-item-avatar tile size="120" color="grey lighten-4">
+                <img :src="require(`../assets/birds/${birds[bird].images[0]}`)" :alt="birds[bird].name" />
+            </v-list-item-avatar>
+        </v-list-item>
+        <v-card-actions>
+            <v-btn text color="primary">Learn more</v-btn>
+        </v-card-actions>
+    </v-card>
 </template>
 
 <script>
@@ -30,31 +25,31 @@ import birds from "@/common/birds.js";
 import common from "@/common/utils.js";
 
 export default {
-  name: "BirdTile",
-  props: ["bird"],
-  data: () => ({
-    birds: birds.data,
-  }),
-  methods: {
-    toKebabCase(bird) {
-      return common.toKebabCase(bird);
+    name: "BirdTile",
+    props: ["bird"],
+    data: () => ({
+        birds: birds.data,
+    }),
+    methods: {
+        toKebabCase(bird) {
+            return common.toKebabCase(bird);
+        },
     },
-  },
 };
 </script>
 
 <style scoped>
 .v-avatar img {
-  object-fit: cover;
+    object-fit: cover;
 }
 .position-absolute {
-  position: absolute;
+    position: absolute;
 }
 .bottom {
-  bottom: 0;
+    bottom: 0;
 }
 .right {
-  right: 0;
+    right: 0;
 }
 </style>
 
