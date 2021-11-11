fb8ce02d00f62b56feb145d33e056f024c0fe0bb
lmilneCC <lmilne@cryptocompare.com>
2021-11-11 22:23:53 +0000

    card backs added

---
+++ src/components/DeckOfCards.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-card rounded>
+        <v-responsive :aspect-ratio="9 / 14">
+            <v-sheet color="secondary" elevation="5" height="100%" class="card-back"></v-sheet>
+        </v-responsive>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: "DeckOfCards",
+};
+</script>
+
+<style scoped>
+.card-back {
+    background-image: url("../assets/images/birds-light.png");
+    background-repeat: repeat;
+}
+</style>
--- src/views/Cards.vue
+++ src/views/Cards.vue
@@ -1,33 +1,40 @@
 <template>
-  <v-container>
-      <p>{{ playerDeck }}</p>
-      <p>{{ houseDeck }}</p>
-  </v-container>
+    <v-container>
+        <v-row>
+            <v-col cols="3"><DeckOfCards /></v-col>
+            <v-col cols="3"><DeckOfCards /></v-col>
+        </v-row>
+        <v-row class="mt-0">
+            <v-col cols-sm="3" offset-sm="6"><DeckOfCards /></v-col>
+            <v-col cols-sm="3"><DeckOfCards /></v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
 // import draggable from "vuedraggable";
 import birds from "@/common/birds.js";
+import DeckOfCards from "../components/DeckOfCards.vue";
 
 export default {
-  name: "Cards",
-  //   components: {
-  //     draggable,
-  //   },
-  data: () => ({
-    birds: birds.data,
-    playerDeck: [],
-    houseDeck: [],
-  }),
-  methods: {
-    sortDecksIntoTwo() {
-      const arrayOfBirds = birds.listOfBirds();
-      this.playerDeck = arrayOfBirds.sort(() => Math.random() - 0.5)
-      this.houseDeck = this.playerDeck.splice(0, arrayOfBirds.length >> 1);
+    name: "Cards",
+    components: {
+        DeckOfCards,
     },
-  },
-  created() {
-    this.sortDecksIntoTwo();
-  },
+    data: () => ({
+        birds: birds.data,
+        playerDeck: [],
+        houseDeck: [],
+    }),
+    methods: {
+        sortDecksIntoTwo() {
+            const arrayOfBirds = birds.listOfBirds();
+            this.playerDeck = arrayOfBirds.sort(() => Math.random() - 0.5);
+            this.houseDeck = this.playerDeck.splice(0, arrayOfBirds.length >> 1);
+        },
+    },
+    created() {
+        this.sortDecksIntoTwo();
+    },
 };
 </script>
