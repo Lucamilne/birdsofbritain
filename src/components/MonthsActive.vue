3c2969214656caefecd9f2761716044d9625e403
lucamilne <lucamilne@googlemail.com>
2021-11-19 16:34:07 +0000

    changed styling of monthsActive

--- src/components/MonthsActive.vue
+++ src/components/MonthsActive.vue
@@ -1,9 +1,23 @@
 <template>
   <v-row no-gutters>
-    <v-col v-for="(month, index) of months" :key="month" cols="3" sm="2" md="1">
-      <v-card tile flat :color="`${monthsActive.includes(index) ? 'secondary' : 'grey'} lighten-3`">
-        <v-card-text :class="`py-3 px-0 caption text-center ${monthsActive.includes(index) ? 'font-weight-black primary--text' : 'text--disabled'}`">{{ month }}</v-card-text>
-        <v-progress-linear absolute bottom :value="monthsActive.includes(index) ? 100 : 0"></v-progress-linear>
+    <v-col v-for="month of months" :key="month" cols="3" sm="2" md="1">
+      <v-card
+        tile
+        flat
+      >
+        <v-card-text
+          :class="`py-1 px-0 overline text-center ${
+            monthsActive.includes(month)
+              ? 'font-weight-black secondary--text'
+              : 'text--disabled'
+          }`"
+          >{{ month }}</v-card-text
+        >
+        <v-progress-linear
+          bottom
+          :value="monthsActive.includes(month) ? 100 : 0"
+          :color="monthsActive.includes(month) ? 'secondary' : 'grey'"
+        ></v-progress-linear>
       </v-card>
     </v-col>
   </v-row>
@@ -14,20 +28,7 @@ export default {
   name: "MonthsActive",
   props: ["monthsActive"],
   data: () => ({
-    months: {
-      jan: "January",
-      feb: "February",
-      mar: "March",
-      apr: "April",
-      may: "May",
-      jun: "June",
-      jul: "July",
-      aug: "August",
-      sep: "September",
-      oct: "October",
-      nov: "November",
-      dec: "December",
-    },
+    months: ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"],
   }),
 };
 </script>
